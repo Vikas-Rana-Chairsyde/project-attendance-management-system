50cc376a75cdc889051b281977fd91afc68d4b5e
Vikas-Rana <vikasrana2510@hotmail.com>
2025-05-05 01:34:42 +0530

    fix: changes for the storybook

--- src/stories/button.stories.tsx
+++ src/stories/button.stories.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Button } from './Button'; // Update the path to your actual component
+import Button from '../component/button'; // Update the path to your actual component
 
 export default {
   title: 'Components/Button', // This is how it shows up in Storybook's sidebar
   component: Button,
 };
 
-export const Primary = () => <Button label="Primary Button" />;
-export const Secondary = () => <Button label="Secondary Button" />;
+export const Primary = () => <Button>Primary Button</Button>;
+export const Secondary = () => <Button>Secondary Button</Button>;
 
