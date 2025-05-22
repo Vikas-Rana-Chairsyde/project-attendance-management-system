2776a94ba4f625b2ac9278ae271fa0bb977d4518
sunali009 <sunali.bhagat@poplify.com>
2025-05-22 22:36:14 +0530

    signin page with constants

---
+++ attendance-management-system/app/pages/signin/constants/constant.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const TEXT = {
+  bigHeading :'Empowering people through seamless HR management.',
+  imagetext : 'Efficiently manage your workforce, streamline operations effortlessly.',
+  signIn : 'Sign In',
+  signInDetails : 'Please enter your details to sign in',
+  labelEmail : 'Email Address',
+  labelPassword : 'Password',
+  rememberMe : 'Remember Me',
+  forgotPassword : 'Forgot Password?',
+};
+
+
--- attendance-management-system/app/pages/signin/page.tsx
+++ attendance-management-system/app/pages/signin/page.tsx
@@ -7,7 +7,8 @@ import hideIcon from "@/public/assets/hide.png";
 import showIcon from "@/public/assets/show.png";
 import logo from "@/public/assets/Logo-Schedura.png";
 import Image from 'next/image';
-import  Styles  from './style/index.module.scss';
+import Styles from './style/index.module.scss';
+import { TEXT } from "@/app/pages/signin/constants/constant";
 
 const SignInForm = () => {
     const initialValues = {
@@ -35,75 +36,76 @@ const SignInForm = () => {
     return (
         <div className={Styles.parentContainer}>
             <div className={Styles.leftSide}>
-                <Image src="https://smarthr.co.in/demo/html/template/assets/img/bg/bg-01.png" alt="background" width={2000}
-                    height={600} className={Styles.spiral}>
-                </Image>
-
-                <div className={Styles.paragraphPart}>
-                    <h1>Empowering people <br />through seamless HR <br />management.</h1>
-                    <div><Image src='https://smarthr.co.in/demo/html/template/assets/img/bg/bg-03.png'
-                        alt="circle-image" width={2000}
-                        height={600} className={Styles.circle}></Image>
+                <div className={Styles.background}>
+                    <Image src="https://smarthr.co.in/demo/html/template/assets/img/bg/bg-01.png" alt="background" width={2000}
+                        height={600} className={Styles.spiral}>
+                    </Image>
+                    <div className={Styles.paragraphPart}>
+                        <h1>{TEXT.bigHeading}</h1>
+                        <div><Image src='https://smarthr.co.in/demo/html/template/assets/img/bg/bg-03.png'
+                            alt="circle-image" width={2000}
+                            height={600} className={Styles.circle}></Image>
+                        </div>
+                        <div><Image src="https://smarthr.co.in/demo/html/template/assets/img/bg/authentication-bg-01.png"
+                            alt="background-image" width={2000}
+                            height={600} className={Styles.people}></Image>
+                        </div>
+                        <p className={Styles.whiteText}>{TEXT.imagetext}</p>
                     </div>
-                    <div><Image src="https://smarthr.co.in/demo/html/template/assets/img/bg/authentication-bg-01.png"
-                        alt="background-image" width={2000}
-                        height={600} className={Styles.people}></Image>
-                    </div>
-                    <p className={Styles.whiteText}>Efficiently manage your workforce, streamline
-                        operations effortlessly.</p>
                     <div><Image src="https://smarthr.co.in/demo/html/template/assets/img/bg/bg-02.png"
-                        alt="circle-image2" width={2000}
-                        height={600} className={Styles.circle_2}></Image>
-                    </div>
-
+                            alt="circle-image2" width={2000}
+                            height={600} className={Styles.circle_2}></Image>
+                        </div>
                 </div>
             </div>
 
 
             <div className={Styles.rightSide}>
+                <div className={Styles.logoIcon}>
+                    <Image src={logo} alt='logo' width={2000}
+                        height={600} className={Styles.logo}></Image></div>
                 <Formik
                     initialValues={initialValues}
                     validationSchema={signInSchema}
                     onSubmit={handleSubmit}>
                     <Form>
-                        {/* <div id='logo'> */}
+                        {/* <div>
                         <Image src={logo} alt='logo' width={2000}
-                            height={600} className={Styles.logo}></Image>
-                        {/* </div> */}
-                        <h2 className={Styles.heading}>Sign In</h2>
-                        <p className={Styles.formHeading}>Please enter your details to sign in</p>
-                      <div className={Styles.mailPass}>  
-                        <div className={Styles.mail}>
-                            <label>Email Address</label>
-                            <Field type="email" name="email"></Field>
-                            <Image src={emailIcon} alt="emailIcon" className={Styles.emailIcon}></Image>
-                            <div className={Styles.error}>
-                                <ErrorMessage name="email" component="div" className={Styles.emailError}/*style={{ color: 'red', fontSize: '12px', marginTop:'3px' }}*//>
+                            height={600} className={Styles.logo}></Image></div> */}
+                        <h2 className={Styles.heading}>{TEXT.signIn}</h2>
+                        <p className={Styles.formHeading}>{TEXT.signInDetails}</p>
+                        <div className={Styles.mailPass}>
+                            <div className={Styles.mail}>
+                                <label>{TEXT.labelEmail}</label>
+                                <Field data-test="email" type="email" name="email"></Field>
+                                <Image src={emailIcon} alt="emailIcon" className={Styles.emailIcon}></Image>
+                                <div className={Styles.error}>
+                                    <ErrorMessage name="email" component="div" className={Styles.emailError}/*style={{ color: 'red', fontSize: '12px', marginTop:'3px' }}*/ />
+                                </div>
                             </div>
-                        </div>
-                        <div className={Styles.password}>
-                            <label>Password</label>
-                            <Field name="password" type={showPassword ? 'text' : 'password'} className={Styles.passwordInput}/>
-                            <Image src= {showPassword ? showIcon : hideIcon } alt="passwordIcon" className={Styles.hide} onClick={togglePassword} ></Image>
-                            <div className={Styles.error2}>
-                                <ErrorMessage name="password" component="div" className={Styles.passwordError}/*style={{ color: 'red', fontSize: '12px', marginTop:'3px' }}*//>
+                            <div className={Styles.password}>
+                                <label>{TEXT.labelPassword}</label>
+                                <Field data-test="password" name="password" type={showPassword ? 'text' : 'password'} className={Styles.passwordInput} />
+                                <Image data-test="passwordIcon" src={showPassword ? showIcon : hideIcon} alt="passwordIcon" className={Styles.hide} onClick={togglePassword} ></Image>
+                                <div className={Styles.error2}>
+                                    <ErrorMessage name="password" component="div" className={Styles.passwordError}/*style={{ color: 'red', fontSize: '12px', marginTop:'3px' }}*/ />
+                                </div>
                             </div>
-                        </div>
                         </div>
                         <div className={Styles.checkbox}>
                             <label className={Styles.remember}>
-                                <Field type="checkbox" name="rememberMe"  className={Styles.accent}/>
-                                Remember Me
+                                <Field data-test="checkbox" type="checkbox" name="rememberMe" className={Styles.accent} />{TEXT.rememberMe}
+
                             </label>
-                            <a href="#">Forgot Password?</a>
+                            <a href="#" data-test="forgotPassword">{TEXT.forgotPassword}</a>
                         </div>
-                        <button className={Styles.button} type='submit'>Sign In</button>
+                        <button data-test="Submit" className={Styles.button} type='submit'>{TEXT.signIn}</button>
 
                         {/* <div className={Styles.account}>
                             <p className={Styles.createAcc}>Don't have an account? <a href="#" className={Styles.underline}>Create Account</a></p>
                         </div> */}
 
-            
+
                         {/* <div id={Styles.or}>
                             <hr style={{ width: "45%", height: "0.01px", color: "rgb(255, 255, 255)" }} />
                             <p>Or</p>
@@ -115,9 +117,10 @@ const SignInForm = () => {
                             <button id={Styles.google}><Image src={googleIcon} alt="googleIcon" /></button>
                             <button id={Styles.apple}><Image src={appleIcon} alt="googleIcon" /></button>
                         </div> */}
-                        
+
                     </Form>
                 </Formik>
+                <div className={Styles.dot}>.</div>
             </div>
         </div>
     );
