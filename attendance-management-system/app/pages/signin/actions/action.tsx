54ef037862fb67fa4a1eb98efb2d76cc43ae34ec
Vikas-Rana <vikasrana2510@hotmail.com>
2025-06-02 09:11:40 +0530

    fix: add comments

--- attendance-management-system/app/pages/signin/actions/action.tsx
+++ attendance-management-system/app/pages/signin/actions/action.tsx
@@ -3,12 +3,14 @@ import React, { useState } from 'react';
 import { signIn } from "next-auth/react";
 import { CONSTANT } from "../constants/constant";
 
+//TODO: Put this in the constants
 const initialValues = {
     email: '',
     password: '',
     rememberMe: false,
 };
 
+//TODO: Add the classes instead of the functions
 export const handleSubmit = async (values: typeof initialValues, { setSubmitting, setErrors }: any) => {
     const res = await signIn("credentials", {
         email: values.email,
