0f0eb933c2c3469fa9ac2a6c6fbabcd05b23335f
sunali009 <sunali.bhagat@poplify.com>
2025-06-03 23:30:49 +0530

    dashboard navbar and profile update page
    navbar of dashboard and UI of profile update page

--- attendance-management-system/app/pages/dashboard/page.tsx
+++ attendance-management-system/app/pages/dashboard/page.tsx
@@ -3,6 +3,10 @@ import React, { useState } from 'react';
 import Styles from "./style/index.module.scss";
 import Image from 'next/image';
 // import logo from "@/public/assets/Logo-Schedura.png";
+import holidayIcon from "@/public/assets/icons8-holiday-24.png";
+import dashboardIcon from "@/public/assets/smart-home.svg";
+import leaveIcon from "@/public/assets/icons8-leave-32.png";
+import attendanceIcon from '@/public/assets/icons8-application-24.png';
 
 export default function Dashboard() {
 
@@ -16,12 +20,33 @@ export default function Dashboard() {
 
         <div className={Styles.optionprofileContainer}>
           <div className={Styles.headerOptions}>
-            <ol className={Styles.list}>
-              <li>Dashboard</li>
-              <li>Holidays</li>
-              <li>Leave</li>
-              <li>Attendance</li>
-            </ol>
+            <ul className={Styles.list}>
+              <li className={Styles.liFlex}>
+                <a href="" className={Styles.active}>
+                  <Image src={dashboardIcon} alt="dashboardIcon" width={2000}
+                    height={600} className={Styles.icon}></Image>
+                  <span>Dashboard</span>
+                </a>
+              </li>
+              <li className={Styles.liFlex}>
+                <Image src={holidayIcon} alt='holidayIcon' width={2000}
+                  height={600} className={Styles.icon}></Image>
+                <span>Holidays</span>
+
+              </li>
+              <li className={Styles.liFlex}>
+                <Image src={leaveIcon} alt='leaveIcon' width={2000}
+                  height={600} className={Styles.icon}></Image>
+                <span>Leave</span>
+
+              </li>
+              <li className={Styles.liFlex}>
+                <Image src={attendanceIcon} alt='attendanceIcon' width={2000}
+                  height={600} className={Styles.icon}></Image>
+                <span>Attendance</span>
+
+              </li>
+            </ul>
           </div>
 
           <div className={Styles.profilePhoto}>
@@ -31,26 +56,64 @@ export default function Dashboard() {
         </div>
       </div>
 
+      {/* mobile sidebar */}
+
       <div className={Styles.mobileHeaderContainer}>
         <div className={Styles.barIcon}>
           <span></span>
+          <span className={Styles.span}></span>
           <span></span>
-          <span></span>
-
         </div>
         <div className={Styles.mobileLogo}><Image src="https://smarthr.co.in/demo/html/template/assets/img/logo.svg" alt='logo' width={2000}
           height={600} className={Styles.logo}></Image>
         </div>
         <div className={Styles.userMenu}>
+          <Image src="https://smarthr.co.in/demo/html/template/assets/img/profiles/avatar-12.jpg" alt="profilePhoto" width={2000}
+            height={600} className={Styles.image} />
+        </div>
+      </div>
+
+      <div className={Styles.sidebarContainer}>
+        <div className={Styles.sidebar}>
+          <div className={Styles.sidebarMenu}>
+            <ul>
+              <li className={Styles.menuTitle}>
+                <span className={Styles.mainMenu}>MAIN MENU</span>
+              </li>
+            </ul>
+            <ul className={Styles.dashoardSection}>
+              <li className={Styles.liFlex}>
+                <a href="" className={Styles.active}>
+                <Image src={dashboardIcon} alt="dashboardIcon" width={2000}
+                    height={600} className={Styles.icon}></Image>
+                  <span>Dashboard</span>
+                </a>
+
+              </li>
+              <li className={Styles.liFlex}>
+                <Image src={holidayIcon} alt='holidayIcon' width={2000}
+                  height={600} className={Styles.icon}></Image>
+                <span>Holidays</span>
+              </li>
+              <li className={Styles.liFlex}>
+                <Image src={leaveIcon} alt='leaveIcon' width={2000}
+                  height={600} className={Styles.icon}></Image>
+                <span>Leave</span>
+              </li>
+              <li className={Styles.liFlex}>
+                <Image src={attendanceIcon} alt='attendanceIcon' width={2000}
+                  height={600} className={Styles.icon}></Image>
+                <span>Attendance</span>
+              </li>
+            </ul>
+          </div>
 
         </div>
-
       </div>
 
     </div>
-
-
   );
+
 }
 
 
